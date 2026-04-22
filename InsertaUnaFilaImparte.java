9035bb9bebd60ee6b0f35c8fc92fcfe50c7ec57f
final0pay <dayronaguilarmeneces1@gmail.com>
2026-04-22 16:40:11 +0200

    g

--- InsertaUnaFilaImparte.java
+++ InsertaUnaFilaImparte.java
@@ -8,15 +8,18 @@ public class InsertaUnaFilaImparte implements DataBaseTask {
     @Override
     public void run(Connection conn, String data)throws BBDDException, SQLException {
         String[] datos = data.split(",");
-        PreparedStatement psI= conn.
-                prepareStatement("INSERT INTO IMPARTE(profesor_id , curso_id , n_modulo , aula_id , fecha) VALUES (?,?,?,?,?)");
+        PreparedStatement psI= conn.prepareStatement("INSERT INTO IMPARTE(profesor_id , curso_id , n_modulo , aula_id , fecha) VALUES (?,?,?,?,?)");
 
         psI.setInt(1,Integer.parseInt(datos[0].trim()));
         psI.setInt(2,Integer.parseInt(datos[1].trim()));
         psI.setInt(3,Integer.parseInt(datos[2].trim()));
         psI.setInt(4,Integer.parseInt(datos[3].trim()));
-        Date fecha= Date.valueOf(datos[4].trim());
-        psI.setDate(5, fecha);
+        String anio =datos[4].split("/")[2].trim();
+        String mes =datos[4].split("/")[1].trim();
+        String dia =datos[4].split("/")[0].trim();
+        String fecha = anio+"-"+mes+"-"+dia;
+        Date fecha2= Date.valueOf(fecha.trim());
+        psI.setDate(5, fecha2);
         int res = psI.executeUpdate();
 
         psI.close();
