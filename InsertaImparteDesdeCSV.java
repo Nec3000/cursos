1a6c0fa4c261ed2374e959016dc254172106a958
final0pay <dayronaguilarmeneces1@gmail.com>
2026-04-28 17:54:22 +0200

    .

--- InsertaImparteDesdeCSV.java
+++ InsertaImparteDesdeCSV.java
@@ -14,8 +14,6 @@ public class InsertaImparteDesdeCSV implements DataBaseTask {
             while(sc.hasNextLine()){
                 String linea = sc.nextLine();
                 String []datos = linea.split(",");
-
-<<<<<<< HEAD
                 try(PreparedStatement ps = conn.prepareStatement("INSERT INTO imparte(profesor_id,curso_id,n_modulo,aula_id,fecha) VALUES(?,?,?,?,?)")) {
                     ps.setInt(1, Integer.parseInt(datos[0].trim()));
                     ps.setInt(2, Integer.parseInt(datos[1].trim()));
@@ -33,18 +31,6 @@ public class InsertaImparteDesdeCSV implements DataBaseTask {
 
                 catch (Exception e) {
                     throw new BBDDException(e,"Insertando");
-=======
-                try(PreparedStatement ps = conn.prepareStatement("INSERT INTO imparte(profesor_id,curso_id,n_modulo,aula_id,fecha) VALUES(?,?,?,?,?)")){
-                ps.setInt(1, Integer.parseInt(datos[0].trim()));
-                ps.setInt(2, Integer.parseInt(datos[1].trim()));
-                ps.setInt(3, Integer.parseInt(datos[2].trim()));
-                ps.setInt(4, Integer.parseInt(datos[3].trim()));
-                Date date = Date.valueOf(datos[4].trim());
-                ps.setDate(5, date);
-                int res = ps.executeUpdate();
-                }catch(Exception e){
-                	e.printStackTrace();
->>>>>>> 7cda0af (prueba)
                 }
             }
         } catch ( Exception e) {
--- InsertaUnaFilaImparte.java
+++ InsertaUnaFilaImparte.java
@@ -8,11 +8,7 @@ public class InsertaUnaFilaImparte implements DataBaseTask {
     @Override
     public void run(Connection conn, String data)throws BBDDException, SQLException {
         String[] datos = data.split(",");
-<<<<<<< HEAD
-        try(PreparedStatement psI= conn.prepareStatement("INSERT INTO imparte(profesor_id , curso_id , n_modulo , aula_id , fecha) VALUES (?,?,?,?,?)")) {
-=======
         try(PreparedStatement psI= conn.prepareStatement("INSERT INTO imparte(profesor_id , curso_id , n_modulo , aula_id , fecha) VALUES (?,?,?,?,?)")){
-
         psI.setInt(1,Integer.parseInt(datos[0].trim()));
         psI.setInt(2,Integer.parseInt(datos[1].trim()));
         psI.setInt(3,Integer.parseInt(datos[2].trim()));
@@ -26,23 +22,6 @@ public class InsertaUnaFilaImparte implements DataBaseTask {
         int res = psI.executeUpdate();
         }catch(Exception e) {
         	e.printStackTrace();
-        }
-
->>>>>>> 7cda0af (prueba)
-
-            psI.setInt(1, Integer.parseInt(datos[0].trim()));
-            psI.setInt(2, Integer.parseInt(datos[1].trim()));
-            psI.setInt(3, Integer.parseInt(datos[2].trim()));
-            psI.setInt(4, Integer.parseInt(datos[3].trim()));
-            String anio = datos[4].split("/")[2].trim();
-            String mes = datos[4].split("/")[1].trim();
-            String dia = datos[4].split("/")[0].trim();
-            String fecha = anio + "-" + mes + "-" + dia;
-            Date fecha2 = Date.valueOf(fecha.trim());
-            psI.setDate(5, fecha2);
-            int res = psI.executeUpdate();
-        } catch (Exception e){
-            e.printStackTrace();
         }
     }
 }
